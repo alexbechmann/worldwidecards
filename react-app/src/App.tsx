8c4c9b0f536cb25414df1a25d41adce61685ea25
Alex Bechmann <alex.bechmann@outlook.com>
2018-03-20 20:16:37 +0100

    added core

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,9 +2,14 @@ import * as React from 'react';
 import './App.css';
 import { Grid } from 'material-ui';
 import { WwcAppBar } from 'src/shared/ui';
+import { Card } from '@core/card';
 
 class App extends React.Component {
   render() {
+    const card: Card = {
+      title: 'test'
+    };
+    console.log(card);
     return (
       <div className="App">
         <WwcAppBar />
