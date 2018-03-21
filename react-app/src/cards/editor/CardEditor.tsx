04403d5c022cb8a9100dfcb06791db0b94e17764
Alex Bechmann <alex.bechmann@outlook.com>
2018-03-21 07:07:21 +0100

    added poc for handling drag end on card editor

--- react-app/src/cards/editor/CardEditor.tsx
+++ react-app/src/cards/editor/CardEditor.tsx
@@ -1,29 +1,40 @@
 import * as React from 'react';
 import { Card } from '@core/index';
-import { Paper } from 'material-ui';
+import { Paper, Grid } from 'material-ui';
 import { Stage, Layer, Text, Rect } from 'react-konva';
 import * as Konva from 'konva';
 
 class ColoredRect extends React.Component {
   state = {
-    color: 'green'
+    color: 'green',
+    x: 0,
+    y: 0
   };
   handleClick = () => {
     this.setState({
       color: Konva.Util.getRandomColor()
     });
   };
+  handleDragEnd = (e: any) => {
+    console.log(e);
+    const mouseEvent: MouseEvent = e.evt;
+    this.setState({
+      x: mouseEvent.layerX,
+      y: mouseEvent.layerY
+    });
+  };
   render() {
     return (
       <Rect
-        x={20}
-        y={20}
+        x={this.state.x}
+        y={this.state.y}
         width={50}
         height={50}
         fill={this.state.color}
         shadowBlur={5}
         onClick={this.handleClick}
         draggable={true}
+        onDragEnd={this.handleDragEnd}
       />
     );
   }
@@ -36,10 +47,14 @@ export interface CardEditorProps {
 export class CardEditor extends React.Component<CardEditorProps> {
   render() {
     return (
-      <Paper>
-        {this.props.card.title}
-        {this.renderCanvas()}
-      </Paper>
+      <Grid container={true}>
+        <Grid item={true}>
+          <Paper>
+            {this.props.card.title}
+            {this.renderCanvas()}
+          </Paper>
+        </Grid>
+      </Grid>
     );
   }
 
