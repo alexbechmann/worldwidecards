b21ac5ec5adbf4eef25c17d17a4044143c83fb4d
Alex Bechmann <alex.bechmann@outlook.com>
2018-03-30 15:41:01 +0200

    prettier

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -11,11 +11,11 @@ import { cardWatcher } from 'src/cards/state/card.watcher';
 
 var config = {
   apiKey: process.env.FIREBASE_CONFIG_API_KEY,
-  authDomain:  process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
-  databaseURL:  process.env.FIREBASE_CONFIG_DATABASE_URL,
+  authDomain: process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_CONFIG_DATABASE_URL,
   projectId: process.env.FIREBASE_CONFIG_PROJECT_ID,
   storageBucket: process.env.FIREBASE_CONFIG_STORAGE_BUCKET,
-  messagingSenderId:  process.env.FIREBASE_CONFIG_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.FIREBASE_CONFIG_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
