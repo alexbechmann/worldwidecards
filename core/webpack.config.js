b334b9a9540e406415f5816f1958b3c710088734
Alex Bechmann <alex.bechmann@outlook.com>
2018-05-12 10:56:44 +0200

    webpack core

---
+++ core/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+function srcPath(subdir) {
+  return path.join(__dirname, "src", subdir);
+}
+
+module.exports = {
+  entry: './src/index.ts',
+  externals: process.env.NODE_ENV === 'production' ? [] : [nodeExternals()],
+  mode: "production",
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  }
+};
